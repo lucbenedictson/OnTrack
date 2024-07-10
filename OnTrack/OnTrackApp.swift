c83bae6a84aa2fae74ddc431e2934b25c1f935f5
Luc Benedictson <lucben2002@gmail.com>
2024-07-10 17:11:15 -0500

    Initial Commit

---
+++ OnTrack/OnTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTrackApp.swift
+//  OnTrack
+//
+//  Created by Luc Benedictson on 2024-07-10.
+//
+
+import SwiftUI
+
+@main
+struct OnTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
